8887d80af7de7986b7efa281d2485e6483210526
RomanFenioux <r.fenioux@hotmail.fr>
2017-10-14 18:58:48 +0200

    dullRazor first implementation. hair mask : done hair mask denoising : in progress (currently replaced by a morpho denoising) interpolation : in progress (a simpler version is implemented) TODO : smoothing/artifact removal

--- code/dullRazor.m
+++ code/dullRazor.m
@@ -3,15 +3,118 @@ function [ Ishaved ] = dullRazor( I )
 %   [ Ishaved ] = dullRazor( I )
 %   I is a grayscale image
 %   Ishaved is the image without the hair
-
-
-    %***** TODO *******
-
-%   In the future dullRazor may require some extra parameters
-
-    
+%
+%   maskThreshold is a threshold used to get the hair mask from the morphological
+%   closing image. It is a heuristic value.
     Ishaved=I;
+    [sizeX, sizeY]=size(I(:,:,1));
 
+    maskThreshold = 0.05;
 
+    %% 1st step : locating dark hair (=computing the hair mask)
+    
+    % we separate the 3 RGB components of I. Each channel will be processed
+    % separately, then the 3 hair masks will be merged
+    Ir = I(:,:,1);
+    Ig = I(:,:,2);
+    Ib = I(:,:,3);
+    
+    % 3 structure elements (vertical, horizontal, 45 degree)
+    SE0  =  [0 1 1 1 1 1 1 0];
+    SE45 =  [0 0 0 0 0 0 0 0;
+             0 1 0 0 0 0 0 0;
+             0 0 1 0 0 0 0 0;
+             0 0 0 1 0 0 0 0;
+             0 0 0 0 1 0 0 0;
+             0 0 0 0 0 1 0 0;
+             0 0 0 0 0 0 1 0;
+             0 0 0 0 0 0 0 0;];
+    SE90 =  [0; 1; 1; 1; 1; 1; 1; 0];
+    
+    % compute the morphological closing on each channel with the 3 SE
+    Irclose = cat(3, imclose(Ir,SE0),imclose(Ir,SE45), imclose(Ir,SE90));
+    Igclose = cat(3, imclose(Ig,SE0),imclose(Ig,SE45), imclose(Ig,SE90));
+    Ibclose = cat(3, imclose(Ib,SE0),imclose(Ib,SE45), imclose(Ib,SE90));
+
+    % compute the closing masks on each channel with a heuristic threshold
+    Gr = abs(Ir-max(Irclose,[],3));
+    Gg = abs(Ig-max(Igclose,[],3));
+    Gb = abs(Ib-max(Ibclose,[],3));
+    
+    Mr = double(Gr>maskThreshold);
+    Mg = double(Gg>maskThreshold);
+    Mb = double(Gb>maskThreshold);
+    
+    % the final mask M is the union of the 3 masks obtained on each channel
+    M=max(cat(3,Mr,Mg,Mb), [], 3);
+    
+    
+    
+    %% 2d step : denoising and interpolation of the removed hair
+    
+    % we check all hair pixels in M to remove the outliers with the function
+    % hairDenoise : if a pixels is not connected to a line (=a hair) then
+    % this pixel is removed from M. See hairDenoise for further details
+    
+    Md=hairDenoise(M);
+    
+    
+    % linear interpolation. To avoid using hair pixel for the
+    % interpolation we give ourselves 4 chances for finding pixels outside 
+    % of the hair. We check the mask in the 4 direction (vertical,
+    % horizontal, both diagonals) for pairs of non-hair pixels.
+    
+    d=20;
+    [row, col]=find(Md==1);
+    for p=1:size(row,1)
+        
+        i=row(p);
+        j=col(p);
+        
+        imin=max(i-d,1);
+        imax=min(i+d,sizeX);
+        jmin=max(j-d,1);
+        jmax=min(j+d,sizeY);
+        Hborder=false;
+        Vborder=false;
+        % border check
+        if (imin~=i-d || imax~=i+d)
+            firstbestneighbor=2; % when border forces horizontal 
+            Hborder=true;
+        end
+        if (jmin~=j-d || jmax~=j+d)
+            firstbestneighbor=1; % when border forces vertical
+            Vborder=true;
+        end    
+        if (Hborder && Vborder)
+            firstbestneighbor=5; % code used to leave I(i,j) as it is
+        end
+        
+        if (~Hborder && ~Vborder) % no border, clear to go            
+            neighbors=[Md(imin,j), Md(i,jmin), Md(imax,jmin), Md(imin,jmax), 0;
+                       Md(imax,j), Md(i,jmax), Md(imin,jmax), Md(imax,jmin), 0];
+
+            % neighbors are valid only if both are outside of the hair : their
+            % positions in the mask should both have a value of 1 (-> we multiply the 2 row)
+            validneighbors=1-neighbors(1,:).*neighbors(2,:);
+            [maxvalue, firstbestneighbor]=max(validneighbors);
+        end
+        
+        switch firstbestneighbor
+            case 1 % vertical neighbors
+                Ishaved(i,j,:)=(I(imin,j,:)+I(imax,j,:))/2;
+            case 2 % horizontal neighbors
+                Ishaved(i,j,:)=(I(i,jmin,:)+I(i,jmax,:))/2;
+            case 3 % left diagonal neighbors
+                Ishaved(i,j,:)=(I(imin,jmin,:)+I(imax,jmax,:))/2;
+            case 4 % right diagonal neighbors
+                Ishaved(i,j,:)=(I(imin,jmax,:)+I(imax,jmin,:))/2;
+            case 5 % border problem or no good neighbor, no interpolation
+                Ishaved(i,j,:)=I(i,j,:);
+        end
+        
+    end
+    
+    
 end
 
--- code/preProc.m
+++ code/preProc.m
@@ -4,37 +4,36 @@ function [ Ipreproc ] = preProc( I, channel )
 %   channel contains a string : 
 %   'meanRGB' average the channels RGB
 %   'b' or 'blue' to select the blue channel in RGB space
-%   'X' ***** TO BE IMPLEMENTED **** the X channel in CIE-XYZ space
+%   'X' to select the X channel in CIE-XYZ space
 %
-%
-%
-%   NOTE :this function may dispapear or require extra parameters in the future
+%   NOTE : this function may require extra parameters in the future
 %   Different segmentation methods will need different preprocessing.
 %   For now preProc merely removes hair, using Dullrazor method and changes
 %   the color channel.
     
-    % averaging the channels R, G, B
+    % removing hair with dullRazor
+    Ishaved = dullRazor(I);
+    
+    % selecting meanRGB : averaging the channels R, G, B
     if strcmp(channel,'meanRGB')
-        I = sum(I,3)/3;
+        Ipreproc = sum(Ishaved,3)/3;
     
     % selecting blue channel
     elseif strcmp(channel, 'b') || strcmp(channel, 'blue')
-        I = I(:,:,3);
+        Ipreproc = Ishaved(:,:,3);
     
     % selecting X channel from CIE-XYZ
     elseif strcmp(channel,'X')
         cR = 0.4125;
         cG = 0.3576;
         cB = 0.1804;
-        I = cR*I(:,:,1) + cG*I(:,:,2) + cB*I(:,:,3);
+        Ipreproc = cR*Ishaved(:,:,1) + cG*Ishaved(:,:,2) + cB*Ishaved(:,:,3);
     
+    % selecting meanRGB by default
     else 
         warning('non existent or invalid channel argument : assumed meanRGB')
-        I = sum(I,3)/3;
+        Ipreproc = sum(Ishaved,3)/3;
     end
-    
-    
-    Ipreproc= dullRazor(I);    
 
 end
 
