38cbead3ab836949e33fc1da492efe6b65c85fef
RomanFenioux <r.fenioux@hotmail.fr>
2017-10-15 10:41:48 +0200

    comments and changes in testOtsu for better readability and clarity

--- code/testOtsu.m
+++ code/testOtsu.m
@@ -4,36 +4,36 @@ close all
 % choose the number of the image (3 last digits)
 imNum = input('image id (3 digits) : ', 's'); 
 
-% choose the paths of the training images and ground truth segmentation masks
-pathTraining = '../data/ISIC-2017_Training_sample/';
-pathTruth = '../data/ISIC-2017_GroundTruth_sample/';
-
+%% read image and ground truth
+% read image, normalize values between 0 and 1, resize (for dullRazor)
+pathIm = '../data/ISIC-2017_Training_sample/';
 imName= strcat('ISIC_0000', imNum, '.jpg');
+I = double(imread(strcat(pathIm, imName)))/255;
+I = imresize(I,[512 nan], 'nearest');
+
+% read groundtruth mask, normalize, resize
+pathTruth = '../data/ISIC-2017_GroundTruth_sample/';
 truthName= strcat('ISIC_0000', imNum, '_segmentation.png');
+T = double(imread(strcat(pathTruth, truthName)))/255;
+T = imresize(T,[512 nan], 'nearest');
 
-%% Segmentation of an image
 
-I = double(imread(strcat(pathTraining, imName)))/255;
-% Image resizing is currently necessary for the dullRazor hair removal 
-I = imresize(I,[512 nan]);
+%% dullRazor
+% hair removal using the dullRazor algorithm. Ishaved and I are RGB images
+Ishaved = dullRazor(I);
 
-% pre-processing the image to change color space and remove hair
-channel='X';
-IpreProc = preProc(I,channel);
+%% channel selection
+% converts Ishaved to a grayscale image (here : channel X from CIE-XYZ)
+channel = 'X';
+IpreProc= channelSelect(Ishaved, channel);
 
-% compute the threshold using Otsu's paper : threshold is the optimal threshold.
-% eta is the separability measure used by Otsu to choose the threshold (see otsu.m), it 
-% can be used to evaluate the quality of the thresholding 
-[threshold, eta] = otsu(IpreProc); 
-
+%% otsu
+% Threshold the image using Otsu's paper : 'threshold' is the optimal threshold.
+% eta is Otsu's separability measure at the optimal threshold. 
+% it can be used to evaluate the quality of the thresholding. 
+[threshold, eta] = otsu(IpreProc);
 I_seuil = double(IpreProc < threshold);
 
-%% Evaluation by displaying the results and the corresponding ground truth mask
-
-T = double(imread(strcat(pathTruth, truthName)))/255;
-% Image resizing is currently necessary for the dullRazor hair removal 
-T = imresize(T, [512 nan]);
-
+%% display
+% display the segmentation and tuth for visual evaluation of the results
 displayResult(IpreProc, I_seuil, T);
-
-
