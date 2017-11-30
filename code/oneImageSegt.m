e20f2d299b4af0b301be8795c63f7e987fe0cf08
RomanFenioux <r.fenioux@hotmail.fr>
2017-11-30 12:41:12 +0100

    postProc.m > compute all post processing , oneImageSegt.m > can select one method or compare the two

--- code/oneImageSegt.m
+++ code/oneImageSegt.m
@@ -4,6 +4,9 @@ clear all
 % choose the number of the image (3 last digits)
 imNum = input('image id (3 digits) : ', 's'); 
 segtMethod = input('segmentation method (otsu or region): ','s');
+computeOtsu = strcmp(segtMethod,'otsu');
+computeRegion = strcmp(segtMethod,'region');
+compare = strcmp(segtMethod,'compare');
 
 %% read image and ground truth
 % read image, normalize values between 0 and 1, resize (for dullRazor)
@@ -41,7 +44,7 @@ blackM = blackFrame(IpreProc,0.2);
 IpreProc=IpreProc-min(IpreProc(~logical(blackM)));
 IpreProc=IpreProc/max(IpreProc(~logical(blackM)));
 
-if strcmp(segtMethod,'otsu')
+if computeOtsu | compare
     %% otsu
     % Threshold the image using Otsu's paper : 'threshold' is the optimal
     % threshold. eta is Otsu's separability measure at the optimal threshold.
@@ -49,9 +52,20 @@ if strcmp(segtMethod,'otsu')
     
     % -2*blackM : negative on the black border region, unchanged elsewhere
     [threshold, eta] = otsu(IpreProc((IpreProc-2*blackM)>0));
-    I_seuil = double(IpreProc < threshold)-blackM;
+    Iotsu = double(IpreProc < threshold)-blackM;
     
-elseif strcmp(segtMethod,'region')
+    %% post processing : 
+    % image filling, connected component analysis (see the function for more
+    % details.
+    IsegtOtsu=postProc(Iotsu);
+    
+        %% evaluation
+    % compute dice and jaccard index
+    dotsu = dice(IsegtOtsu, T);
+    jotsu = jaccard(IsegtOtsu,T);
+end
+    
+if computeRegion | compare
     %% Region Growing
     % start from a seed and add neighbor pixels to the region as long as
     % their intensity is close (threshold) to the mean intensity of the region
@@ -60,48 +74,31 @@ elseif strcmp(segtMethod,'region')
     if numel(regionInputs)==0
         regionInputs=[round(size(I,1)/2),round(size(I,2)/2),0.2]
     end
-    I_seuil=regionGrowing(IpreProc,256,340);
+    Iregion=regionGrowing(IpreProc,regionInputs(1),regionInputs(2),regionInputs(3));
+    
+    %% post processing : 
+    % image filling, connected component analysis (see the function for more
+    % details.
+    IsegtRegion=postProc(Iregion);    
+    
+    %% evaluation
+    % compute dice and jaccard index
+    dregion = dice(IsegtRegion, T);
+    jregion = jaccard(IsegtRegion,T);
 end
 
-%% Image filling
-%fill the holes in the regions
-I_filled=imfill(I_seuil,'holes');
-
-%% Connected component analysis
-% connected component
-CC=bwconncomp(I_filled);
-% Compute the areas and the bounding box of each component
-stats=regionprops('table',CC,'Area','BoundingBox','Centroid');
-% keep only the components with a sufficient area and centered (heuristic,
-% but this is not very sensitive, because lesions are way bigger, and noise
-% is way smaller than 80)
-center=repmat(size(I_filled)/2,size(stats,1),1);
-distance=sqrt(sum((stats.Centroid-center).^2,2));
-idx=find([stats.Area]>1000 &  ...
-       stats.BoundingBox(:,1)>2 & stats.BoundingBox(:,2)>2 & ...
-       stats.BoundingBox(:,1)+stats.BoundingBox(:,3)<size(I_filled,2)-1 & ...
-       stats.BoundingBox(:,2)+stats.BoundingBox(:,4)<size(I_filled,1)-1);
-   
-if numel(idx)==0
-    idx=find([stats.Area]>1000);
-end
-
-% To choose among the big areas, we keep those with a small bounding box
-% (this avoids choosing the black margins)
-%boundingBoxSizes=max([stats.BoundingBox(:,3), stats.BoundingBox(:,4)],[],2);
-%[~,argmin]=min(boundingBoxSizes(idx));
-Isegt=double(ismember(labelmatrix(CC),idx));
-
-
-%% evaluation
-% compute dice and jaccard index
-d = dice(Isegt, T);
-j = jaccard(Isegt,T);
 
 %% display
 % display the segmentation and tuth for visual evaluation of the results
-displayResult(IpreProc, Isegt, T);
-title(sprintf('Otsu Threshold on image %s : dice = %g, jaccard = %g',imNum,d,j))
-
+if compare
+    displayResult(IpreProc, T, IsegtOtsu, IsegtRegion)
+    title(sprintf('comparison between the segmentation methods'))
+elseif computeOtsu
+    displayResult(IpreProc, T, IsegtOtsu);
+    title(sprintf('Otsu Threshold on image %s : dice = %g, jaccard = %g',imNum,dotsu,jotsu))
+elseif computeRegion
+    displayResult(IpreProc, T, IsegtRegion);
+    title(sprintf('Region Growing on image %s : dice = %g, jaccard = %g',imNum,dregion,jregion))
+end
 
 
