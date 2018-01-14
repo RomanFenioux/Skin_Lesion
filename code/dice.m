87883c70998a0544fd1816490ba0deb0b36a8a87
fenioux <roman.fenioux@telecom-paristech.fr>
2018-01-14 16:34:06 +0100

    added function for compatibility with matlab R2015

---
+++ code/dice.m
@@ -0,0 +1,7 @@
+function [ d ] = dice( segIm, grndTruth )
+%DICE(I,J) Computes the dice index between binary images of same size
+
+    d = 2*nnz(segIm & grndTruth)/(nnz(segIm) + nnz(grndTruth));
+
+end
+
